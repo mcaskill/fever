02f9864d10680e889db6e1c15d5d52bf609e2561
Chauncey McAskill <chauncey@mcaskill.ca>
2023-02-04 23:28:10 -0500

    Update DOCTYPE and HTML
    Changed:
    - Removed XMLNS namespace.
    - Replaced "Content-Type" meta with `charset`.

--- firewall/app/views/default/reader/header.php
+++ firewall/app/views/default/reader/header.php
@@ -1,8 +1,7 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
-	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<!DOCTYPE html>
+<html lang="en">
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+<meta charset="utf-8" />
 <title>Fever&deg;<?php if ($this->prefs['unread_counts'] && $this->total_unread):?> (<?php e($this->total_unread); ?>)<?php endif; ?></title>
 <meta http-equiv="X-UA-Compatible" content="chrome=1" />
 <link rel="apple-touch-icon" href="firewall/app/images/webclip.png" />
--- firewall/app/views/mobile/page/header.php
+++ firewall/app/views/mobile/page/header.php
@@ -1,8 +1,7 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
-	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<!DOCTYPE html>
+<html lang="en">
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+<meta charset="utf-8" />
 <title>Fever&deg;</title>
 <meta name="viewport" id="viewport" content="initial-scale=1.0,user-scalable=no,minimum-scale=1.0,maximum-scale=1.0" />
 <meta name="apple-mobile-web-app-capable" content="yes" />
